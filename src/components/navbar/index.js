baa0cd6a311975a4de8ae63c16db9884836bb3d9
Rjolaverria <37763385+Rjolaverria@users.noreply.github.com>
2020-11-23 10:57:33 -0500

    Added signin/signout

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,10 +1,12 @@
 import { useContext, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     AppBar,
     Toolbar,
     Typography,
     IconButton,
+    Button,
     Menu,
     MenuItem,
     Avatar,
@@ -45,6 +47,53 @@ const Navbar = () => {
         handleClose();
     };
 
+    const userNav = auth.currentUser && (
+        <div>
+            <IconButton
+                aria-label='account of current user'
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+                onClick={handleMenu}
+                color='inherit'
+            >
+                <Avatar
+                    alt={auth.currentUser.displayName}
+                    src={auth.currentUser.photoURL}
+                />
+            </IconButton>
+            {auth.currentUser && (
+                <Menu
+                    id='menu-appbar'
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <MenuItem onClick={handleSignOut}>Sign out</MenuItem>
+                </Menu>
+            )}
+        </div>
+    );
+
+    const guestNav = (
+        <div>
+            <Button component={Link} to='/login'>
+                Login
+            </Button>
+            <Button component={Link} to='/register'>
+                Sign Up
+            </Button>
+        </div>
+    );
+
     return (
         <div className={classes.root}>
             <AppBar position='fixed' className='navbar'>
@@ -54,43 +103,7 @@ const Navbar = () => {
                             <WhatshotIcon color='secondary' />
                             FireChat
                         </Typography>
-                        {auth.currentUser && (
-                            <div>
-                                <IconButton
-                                    aria-label='account of current user'
-                                    aria-controls='menu-appbar'
-                                    aria-haspopup='true'
-                                    onClick={handleMenu}
-                                    color='inherit'
-                                >
-                                    <Avatar
-                                        alt={auth.currentUser.displayName}
-                                        src={auth.currentUser.photoURL}
-                                    />
-                                </IconButton>
-                                {auth.currentUser && (
-                                    <Menu
-                                        id='menu-appbar'
-                                        anchorEl={anchorEl}
-                                        anchorOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'left',
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: 'top',
-                                            horizontal: 'right',
-                                        }}
-                                        open={open}
-                                        onClose={handleClose}
-                                    >
-                                        <MenuItem onClick={handleSignOut}>
-                                            Sign out
-                                        </MenuItem>
-                                    </Menu>
-                                )}
-                            </div>
-                        )}
+                        {auth.currentUser ? userNav : guestNav}
                     </Toolbar>
                 </Container>
             </AppBar>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,8 +9,10 @@ import {
     Grid,
     Typography,
     Container,
+    SvgIcon,
 } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
+import FacebookIcon from '@material-ui/icons/Facebook';
 
 import { FirebaseContext } from '../context/firebaseContext';
 
@@ -42,6 +44,17 @@ const Login = () => {
             setTimeout(() => setError(''), 5000);
         });;
     };
+
+    const googleIcon = (
+        <SvgIcon className='button-icon'>
+            <svg>
+                <path
+                    fill='currentColor'
+                    d='M21.35,11.1H12.18V13.83H18.69C18.36,17.64 15.19,19.27 12.19,19.27C8.36,19.27 5,16.25 5,12C5,7.9 8.2,4.73 12.2,4.73C15.29,4.73 17.1,6.7 17.1,6.7L19,4.72C19,4.72 16.56,2 12.1,2C6.42,2 2.03,6.8 2.03,12C2.03,17.05 6.16,22 12.25,22C17.6,22 21.5,18.33 21.5,12.91C21.5,11.76 21.35,11.1 21.35,11.1V11.1Z'
+                />
+            </svg>
+        </SvgIcon>
+    );
 
     if (auth.currentUser) {
         return <Redirect to='/' />;
@@ -114,6 +127,7 @@ const Login = () => {
                         color='secondary'
                         onClick={googleSignIn}
                     >
+                        {googleIcon}
                         Sign In with Google
                     </Button>
                     <Button
@@ -122,6 +136,7 @@ const Login = () => {
                         className='facebook-button'
                         onClick={fbSignIn}
                     >
+                        <FacebookIcon className='button-icon' />
                         Sign In with Facebook
                     </Button>
                 </form>
